453f0aa8f2e7910192cd609b8b5bc07268960f2e
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-01-05 04:32:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,18 +32,21 @@ end
     def self.new_by_filename(filename)
       song_name = filename.partition(" - ")[2]
       song_name2= /[a-zA-Z ]+-/.match(song_name).to_s.gsub(" -", "")
+        artist_name_formatted = filename.partition(" - ")[0]
       song_name2 = Song.new(song_name2)
-      artist_name_formatted = filename.partition(" - ")[0]
+      @artist=(artist_name_formatted)
+      
       #binding.pry
       artist_name=(artist_name_formatted)
-      song_name2.artist= artist_name_formatted
-      song_name2
+      
+                                 song_name2.artist= artist_name_formatted
+      song_name2  
     end
     
     def artist_name=(artistname) 
 		found = Artist.all.detect{|el| el.name == artistname}
 			 if found ==true
-			   self.artist=found
+			   self.artist=found  
 					artistname.songs << self
 			 elsif 	found != true
 				 artistname = Artist.new(artistname)
